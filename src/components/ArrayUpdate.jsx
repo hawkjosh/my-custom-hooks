67e913911ce14b244700f0fd629e16b5568ef685
hawkjosh <hawkjosh@icloud.com>
2023-11-30 16:22:21 -0500

    more work on ArrayUpdate.jsx

--- src/components/ArrayUpdate.jsx
+++ src/components/ArrayUpdate.jsx
@@ -28,7 +28,7 @@ const InputModal = ({ openModal, closeModal, children }) => {
 
 	return (
 		<dialog
-			className='w-3/4 p-8 m-auto border-2 rounded-lg'
+			className='w-1/2 p-8 m-auto border-2 rounded-lg'
 			ref={ref}
 			onCancel={closeModal}>
 			{children}
@@ -41,7 +41,7 @@ const InputModal = ({ openModal, closeModal, children }) => {
 	)
 }
 
-const InputForm = ({ onFormSubmit }) => {
+const InputForm = ({ array, onFormSubmit }) => {
 	const [option, setOption] = useState('')
 	const [idx1, setIdx1] = useState('')
 	const [idx2, setIdx2] = useState('')
@@ -49,39 +49,54 @@ const InputForm = ({ onFormSubmit }) => {
 	const [showIdx1, setShowIdx1] = useState(false)
 	const [showIdx2, setShowIdx2] = useState(false)
 	const [showVal, setShowVal] = useState(false)
-	const [showSubmit, setShowSubmit] = useState(false)
 
 	const options = [
-		'add a value to end',
-		'remove last value',
+		'add value before first',
+		'add value after last',
+		'add value elsewhere',
 		'remove first value',
-		'add a value to start',
-    'add a value',
-    'remove a value',
-		'fill with a value',
-		'change a value',
+		'remove last value',
+		'remove other value',
+		'fill with same value',
+		'change some value',
 		'swap two values',
 	]
 
+	const resetForm = () => {
+		setOption('')
+		setIdx1('')
+		setIdx2('')
+		setVal('')
+		setShowIdx1(false)
+		setShowIdx2(false)
+		setShowVal(false)
+	}
+
 	const handleOptionChange = (e) => {
+		resetForm()
 		const choice = e.target.value
 		setOption(choice)
-		if (
-			choice === 'add a value' ||
-			choice === 'remove a value' ||
-			choice === 'change a value' ||
-			choice === 'swap two values'
+		if (choice === 'remove other value') {
+			setShowIdx1(true)
+		} else if (
+			choice === 'add value elsewhere' ||
+			choice === 'change some value'
 		) {
 			setShowIdx1(true)
+			setShowVal(true)
 			return
 		} else if (
-			choice === 'add a value to end' ||
-			choice === 'add a value to start' ||
-			choice === 'fill with a value'
+			choice === 'add value before first' ||
+			choice === 'add value after last' ||
+			choice === 'fill with same value'
 		) {
 			setShowVal(true)
 			return
-		} else setShowSubmit(true)
+		} else if (choice === 'swap two values') {
+			setShowIdx1(true)
+			setShowIdx2(true)
+			return
+		}
 	}
 
 	const handleIndexChange = (e) => {
@@ -93,13 +108,14 @@ const InputForm = ({ onFormSubmit }) => {
 			return
 		}
 		tName === 'idx1' ? setIdx1(parseInt(tVal)) : setIdx2(parseInt(tVal))
-		if (option === 'add a value' || option === 'fill with a value' || option === 'change a value') {
+		if (
+			option === 'add value elsewhere' ||
+			option === 'fill with same value' ||
+			option === 'change some value'
+		) {
 			setShowVal(true)
 			return
-		} else if (option === 'swap two values') {
-			setShowIdx2(true)
-			return
-		} else setShowSubmit(true)
+		}
 	}
 
 	const handleValueChange = (e) => {
@@ -109,17 +125,6 @@ const InputForm = ({ onFormSubmit }) => {
 			setVal('')
 			return
 		} else setVal(parseInt(tVal))
-		setShowSubmit(true)
-	}
-
-	const resetForm = () => {
-		setOption('')
-		setIdx1('')
-		setIdx2('')
-		setVal('')
-		setShowIdx1(false)
-		setShowVal(false)
-		setShowSubmit(false)
 	}
 
 	const handleSubmit = (e) => {
@@ -134,21 +139,18 @@ const InputForm = ({ onFormSubmit }) => {
 				className='flex flex-col gap-4'
 				onSubmit={handleSubmit}>
 				<div className='flex flex-col gap-1'>
-					<label
-						htmlFor='option'
-						className=''>
-						What would you like to do?
-					</label>
+					<label htmlFor='option'>What would you like to do?</label>
 					<select
 						name='option'
 						id='option'
 						value={option}
 						onChange={handleOptionChange}
-						className='w-full text-2xl text-center border-2 border-black rounded-lg '>
+						className='text-2xl text-center border-2 border-black rounded-lg w-max pe-4'>
 						<option
 							value=''
-							disabled
-						/>
+							disabled>
+							Select option
+						</option>
 						{options.map((option, index) => (
 							<option
 								key={index}
@@ -159,64 +161,91 @@ const InputForm = ({ onFormSubmit }) => {
 						))}
 					</select>
 				</div>
-				{showIdx1 && (
-					<div className='flex items-center gap-2'>
-						<label
-							htmlFor='idx1'
-							className=''>
-							At which index?
-						</label>
-						<input
-							type='text'
-							id='idx1'
-							name='idx1'
-							value={idx1}
-							onChange={handleIndexChange}
-							className='w-10 h-10 text-2xl text-center border-2 border-black rounded-lg'
-						/>
-					</div>
-				)}
-				{showIdx2 && (
-					<div className='flex items-center gap-2'>
-						<label
-							htmlFor='idx2'
-							className=''>
-							At which other index?
-						</label>
-						<input
-							type='text'
-							id='idx2'
-							name='idx2'
-							value={idx2}
-							onChange={handleIndexChange}
-							className='w-10 h-10 text-2xl text-center border-2 border-black rounded-lg'
-						/>
-					</div>
-				)}
-				{showVal && (
-					<div className='flex items-center gap-2'>
-						<label
-							htmlFor='val'
-							className=''>
-							With what value?
-						</label>
-						<input
-							type='text'
-							id='val'
-							name='val'
-							value={val}
-							onChange={handleValueChange}
-							className='w-10 h-10 text-2xl text-center border-2 border-black rounded-lg'
-						/>
-					</div>
-				)}
-				{showSubmit && (
-					<input
-						type='submit'
-						value='Submit'
-						className='w-1/2 m-auto font-bold border-2 border-black rounded-lg h-1/4'
-					/>
-				)}
+				<div className='flex items-center gap-12 mb-12'>
+					{showIdx1 && (
+						<div className='flex flex-col gap-1'>
+							<label htmlFor='idx1'>
+								{option === 'add value elsewhere'
+									? 'Before which position?'
+									: option === 'remove other value' ||
+									  option === 'change some value'
+									? 'At which position?'
+									: option === 'swap two values'
+									? '1st position?'
+									: ''}
+							</label>
+							<select
+								name='idx1'
+								id='idx1'
+								value={idx1}
+								onChange={handleIndexChange}
+								className='text-2xl text-center border-2 border-black rounded-lg w-max pe-4'>
+								<option
+									value=''
+									disabled>
+									Select position
+								</option>
+								{array.map((_, index) => (
+									<option
+										key={index}
+										value={index}
+										className='text-black'>
+										{index + 1}
+									</option>
+								))}
+							</select>
+						</div>
+					)}
+					{showIdx2 && (
+						<div className='flex flex-col gap-1'>
+							<label htmlFor='idx2'>2nd position?</label>
+							<select
+								name='idx2'
+								id='idx2'
+								value={idx2}
+								onChange={handleIndexChange}
+								className='text-2xl text-center border-2 border-black rounded-lg w-max pe-4'>
+								<option
+									value=''
+									disabled>
+									Select position
+								</option>
+								{array.map((_, index) => (
+									<option
+										key={index}
+										value={index}
+										className='text-black'>
+										{index + 1}
+									</option>
+								))}
+							</select>
+						</div>
+					)}
+					{showVal && (
+						<div className='flex flex-col gap-1'>
+							<label htmlFor='val'>What value?</label>
+							<input
+								type='text'
+								id='val'
+								name='val'
+								value={val}
+								onChange={handleValueChange}
+								className='w-40 text-2xl text-center border-2 border-black rounded-lg'
+							/>
+						</div>
+					)}
+				</div>
+				<input
+					type='submit'
+					value='Submit'
+					disabled={
+						option === '' ||
+						(showIdx1 && idx1 === '') ||
+						(showIdx2 && idx2 === '') ||
+						(showVal && val === '')
+					}
+					className='w-1/4 m-auto font-bold text-white bg-green-500 border-2 border-black rounded-lg cursor-pointer disabled:text-gray-400 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-black disabled:hover:text-white disabled:bg-white'
+				/>
 			</form>
 		</div>
 	)
@@ -224,35 +253,35 @@ const InputForm = ({ onFormSubmit }) => {
 
 export default function ArrayUpdate() {
 	const [modal, setModal] = useState(false)
-	const { array, methods } = useArray([1, 2, 3, 4, 5])
+	const { array, methods } = useArray()
 
 	const handleModalOpen = () => setModal(true)
 	const handleModalClose = () => setModal(false)
 
 	const handleFormSubmit = (option, idx1, idx2, val) => {
 		switch (option) {
-			case 'add a value to end':
+			case 'add value before first':
+				methods.unshift(val)
+				break
+			case 'add value after last':
 				methods.push(val)
 				break
-			case 'remove last value':
-				methods.pop()
+			case 'add value elsewhere':
+				methods.splice(idx1, 0, val)
 				break
 			case 'remove first value':
 				methods.shift()
 				break
-			case 'add a value to start':
-				methods.unshift(val)
+			case 'remove last value':
+				methods.pop()
 				break
-			case 'add a value':
-				methods.splice(idx1, 0, val)
-				break
-			case 'remove a value':
+			case 'remove other value':
 				methods.splice(idx1, 1)
 				break
-			case 'fill with a value':
+			case 'fill with same value':
 				methods.fill(val)
 				break
-			case 'change a value':
+			case 'change some value':
 				methods.update(idx1, val)
 				break
 			case 'swap two values':
@@ -288,32 +317,37 @@ export default function ArrayUpdate() {
 			<InputModal
 				openModal={modal}
 				closeModal={handleModalClose}>
-				<InputForm onFormSubmit={handleFormSubmit} />
+				<InputForm
+					array={array}
+					onFormSubmit={handleFormSubmit}
+				/>
 			</InputModal>
 		</div>
 	)
 }
 
-function useArray(arr) {
-	const [array, setArray] = useState(arr)
+function useArray() {
+	const initialArray = [1, 2, 3, 4, 5]
+	const [array, setArray] = useState(initialArray)
 	const methods = {
-		push: (el) => setArray((arr) => [...arr, el]),
-    pop: () => setArray((arr) => arr.slice(0, arr.length - 1)),
-    shift: () => setArray((arr) => arr.slice(1)),
-    unshift: (el) => setArray((arr) => [el, ...arr]),
-    splice: (index, count, ...elements) => setArray((arr) => [...arr.slice(0, index), ...elements, ...arr.slice(index + count)]),
-		remove: (index) =>
-			setArray((arr) => [...arr.slice(0, index), ...arr.slice(index + 1)]),
-		// filter: (callback) => setArray((arr) => arr.filter(callback)),
-    fill: (value) => setArray((arr) => arr.map(() => value)),
-		update: (index, newElement) =>
-			setArray((arr) => [
-				...arr.slice(0, index),
-				newElement,
-				...arr.slice(index + 1),
+		push: (value) => setArray([...array, value]),
+		pop: () => setArray(array.slice(0, array.length - 1)),
+		shift: () => setArray(array.slice(1)),
+		unshift: (value) => setArray([value, ...array]),
+		splice: (index, count, ...values) =>
+			setArray([
+				...array.slice(0, index),
+				...values,
+				...array.slice(index + count),
 			]),
-		swap: (idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]),
-		reset: () => setArray(arr),
+		remove: (index) =>
+			setArray([...array.slice(0, index), ...array.slice(index + 1)]),
+		fill: (value) => setArray(array.map(() => value)),
+		update: (index, value) =>
+			setArray([...array.slice(0, index), value, ...array.slice(index + 1)]),
+		swap: (index1, index2) =>
+			([array[index1], array[index2]] = [array[index2], array[index1]]),
+		reset: () => setArray(initialArray),
 	}
 	return { array, methods }
 }
